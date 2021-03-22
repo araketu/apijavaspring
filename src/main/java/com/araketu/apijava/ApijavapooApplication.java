5ed3a00879debdceff304affa2d119fe48220563
araketu <diogo.ghizoni@outlook.com>
2021-03-22 10:01:12 -0300

    First commit - API Project

---
+++ src/main/java/com/araketu/apijava/ApijavapooApplication.java
@@ -0,0 +1,13 @@
+package com.araketu.apijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApijavapooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApijavapooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/araketu/apijava/ApijavapooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.araketu.apijava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApijavapooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
